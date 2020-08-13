760c146cccac3f60002dee29d0078ecb0aec3b81
cnahuina <cnahuina@gmail.com>
2020-08-13 12:31:24 -0500

    upload

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,6 +1,7 @@
 export var GLOBAL = {
-    // url: "http://localhost:4000/api", //local
-    // image: "http://localhost:4000/uploads/"
-    url: "https://api.lavanderiatotalcleaning.com/api", //local
-    image: "https://api.lavanderiatotalcleaning.com/uploads/"
+    url: "http://localhost:4000/api", //local
+    image: "http://localhost:4000/uploads/"
+
+    // url: "https://api.lavanderiatotalcleaning.com/api", //PRD
+    // image: "https://api.lavanderiatotalcleaning.com/uploads/"
 }
